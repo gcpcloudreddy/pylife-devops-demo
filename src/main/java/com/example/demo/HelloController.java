e356b1465561cf1065b99cf123fd088f051ebde3
gcpcloudreddy <83153921+gcpcloudreddy@users.noreply.github.com>
2023-04-08 19:38:27 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Python Life DevOPS Class Today - March 29 2023 !";
+        return "Greetings from Python Life DevOPS Class Today - Apr 8 2023 !";
     }
 
 }
